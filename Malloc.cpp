146a150f2afd3ffcca23166dc4a6ac6396a85b46
BlessedForever04 <vinitwankhade411@gmail.com>
2025-01-08 12:51:23 +0530

    minor change

--- Malloc.cpp
+++ Malloc.cpp
@@ -1,16 +1,15 @@
 // There's no need to include any external header file to use malloc() and free() function for dynamic memory allocation.
 #include <iostream>
-using namespace std;
 
 // It is a basic program for storing Ids. (dynamically)
 int main(){
-    cout<<"Enter the number of Id's you want to save: ";
+    std::cout<<"Enter the number of Id's you want to save: ";
     int number; // Number of ids
-    cin>>number;
+    std::cin>>number;
 
     // Validating the input for input 0 and less than 0
     if(number <= 0){
-        cout<<"Invalid input\n";
+        std::cout<<"Invalid input\n";
     }
 
     // Malloc implementation, malloc - Memory Allocation.
@@ -18,20 +17,20 @@ int main(){
 
     // Malloc failure check
     if(ptr == NULL){
-        cout<<"Malloc failed, either heap is full or can't access it";
+        std::cout<<"Malloc failed, either heap is full or can't access it";
     }
 
     // User inputs for Ids
     for(int i = 0; i < number; i++){
-        cout<<"Enter Id number "<<i+1<<": ";
-        cin>>ptr[i];
+        std::cout<<"Enter Id number "<<i+1<<": ";
+        std::cin>>ptr[i];
     }
 
-    cout<<endl;
+    std::cout<<std::endl;
 
     // Printing all ids
     for(int i = 0; i < number; i++){
-        cout<<"Id number "<<i+1<<" is: "<<ptr[i]<<endl;
+        std::cout<<"Id number "<<i+1<<" is: "<<ptr[i]<<std::endl;
     }
 
     // Freeing the memory allocated to avoid memory leak.
